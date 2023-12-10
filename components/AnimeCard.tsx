345f28cb68161099cae49fe9722029e13a5c0178
Tamer Osman <tamer4osman@gmail.com>
2023-12-10 23:16:28 +0100

    Fix layout issue

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -15,8 +15,8 @@ export interface AnimeProp {
     original: string;
   };
   kind: string;
-  episodes: number;
-  episodes_aired: number;
+  episodes: number | null;
+  episodes_aired: number | null;
   score: string;
 }
 
@@ -25,7 +25,9 @@ interface Prop {
   index: number;
 }
 
-const AnimeCard =({ anime: { name, kind, episodes, episodes_aired, score, image }, index }: Prop) =>{
+const AnimeCard = ({ anime, index }: Prop) => {
+  const { name, kind, episodes, episodes_aired, score, image } = anime;
+
   return (
     <MotionDiv
       variants={variants}
@@ -39,34 +41,35 @@ const AnimeCard =({ anime: { name, kind, episodes, episodes_aired, score, image 
       viewport={{ amount: 0 }}
       className="max-w-sm rounded relative w-full"
     >
-      <div className="relative w-full h-[37vh]">
-        <Image src={`https://shikimori.one${image.original}`} alt={name} fill className="rounded-xl" />
+      <div className="relative w-full h-[37vh] rounded-xl overflow-hidden">
+        <Image src={`https://shikimori.one${image.original}`} alt={name} layout="fill" objectFit="cover" />
       </div>
-      <div className="py-4 flex flex-col gap-3">
+      <div className="py-4 px-3 flex flex-col gap-3 bg-[#161921]">
         <div className="flex justify-between items-center gap-1">
           <h2 className="font-bold text-white text-xl line-clamp-1 w-full">
             {name}
           </h2>
-          <div className={`py-1 px-2 bg-[#161921] rounded-sm text-white text-sm font-bold capitalize`}>
+          <div className="py-1 px-2 rounded-sm text-white text-sm font-bold capitalize">
             {kind}
           </div>
         </div>
         <div className="flex gap-4 items-center">
           {episodes || episodes_aired ? (
             <div className="flex flex-row gap-2 items-center">
-              <Image src="./episodes.svg" alt="episodes" width={20} height={20} className="object-contain" />
+              <Image src="/episodes.svg" alt="episodes" width={20} height={20} className="object-contain" />
               <p className="text-base text-white font-bold">
                 {episodes || episodes_aired}
               </p>
             </div>
           ) : null}
           <div className="flex flex-row gap-2 items-center">
-            <Image src="./star.svg" alt="star" width={18} height={18} className="object-contain" />
+            <Image src="/star.svg" alt="star" width={18} height={18} className="object-contain" />
             <p className="text-base font-bold text-[#FFAD49]">{score}</p>
           </div>
         </div>
       </div>
     </MotionDiv>
   );
-}
+};
+
 export default AnimeCard;
